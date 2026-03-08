83d2661ff40c02ff31bb9b0d84505e0fe63bcd81
Ajijur Rahman <arahman.codes@gmail.com>
2026-03-09 01:04:47 +0530

    this is solution of leetcode 713 number problem

---
+++ Array/Sort_color.java
@@ -0,0 +1,6 @@
+public class Sort_color {
+    public static void main(String[] args){ 
+        int[] nums = { 2,0,1,2,1,0,0,2,1};
+
+    }
+}
---
+++ Sliding_Windows/Leetcode_Seven_One_three.java
@@ -0,0 +1,21 @@
+package Sliding_Windows;
+public class Leetcode_Seven_One_three {
+    public static void main(String[] args) {
+        int[] nums = {5,2,6,1,4,2,3,7,2,1,5,6,2,3,1,4,2,6,3,5,2,1,4,3,2,6,1,5,3};
+        int k = 50;
+        int si = 0;
+        int ei = 0;
+        int ans = 0;
+        int p = 1;
+        while(ei<nums.length){
+            p *= nums[ei];
+            while(p >= k && si <= ei){
+                p /= nums[si];
+                si++;
+            }
+            ans += (ei-si+1);
+            ei++;
+        }
+        System.out.println(ans);
+    }
+}
